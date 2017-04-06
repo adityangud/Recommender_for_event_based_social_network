3954ad2a40c59afb7726369c5c63c11fbe64b267
navneet gupta <navgupta14@gmail.com>
2017-04-06 15:24:10 -0500

    fixing minor bug, parameter wasn't passed

--- src/data-model/event_info.py
+++ src/data-model/event_info.py
@@ -1,7 +1,7 @@
 
 class EventInfo:
 
-    def __init__(self, event_id, description, locat):
+    def __init__(self, event_id, description, locat, group):
         self.event_id = event_id
         self.description = description
         self.locat = locat
