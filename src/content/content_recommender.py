9a9b4cf2f8a7e8f98533d70010a8d7d7013c0e39
navneet gupta <navgupta14@gmail.com>
2017-04-06 15:46:35 -0500

    adding description of ToDo for content based recommendation.

--- src/content/content_recommender.py
+++ src/content/content_recommender.py
@@ -3,11 +3,15 @@ class ContentRecommender:
     def __init__(self):
         pass
 
+#TODO - Fit - form a vocab using training events(all valid events).
+#TODO - Ideas for vocab (1 gram, 2 gram or ngram).
     def train(self, training_events):
         ## input : dict([user, list of events])
         ## output : dict([user, tfidf - vector])
         pass
 
+#TODO - Transform - form vectors for a user's past events. Sum these vectors to form user vector. Calculate similarity scores and rank events.
+#TODO - Ideas for summing (direct sum, weighted sum(timed, event_importance)).
     def test(self, member_id, potential_events):
         ## input : member_id, list_of_events
         ## output : [event : cosine similarity score]
