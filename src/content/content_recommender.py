f1bce2b2a7aaef0cabdf008998dc3b768a4aafb6
navneet gupta <navgupta14@gmail.com>
2017-04-06 20:58:44 -0500

    changing code to comply with data sent in intended data structures.

--- src/content/content_recommender.py
+++ src/content/content_recommender.py
@@ -8,27 +8,29 @@ class ContentRecommender:
 
 #TODO - Fit - form a vocab using training events(all valid events).
 #TODO - Ideas for vocab (1 gram, 2 gram or ngram).
-    def train(self, training_events):
-        ## input : dict([user, list of events])
-        ## output : dict([user, tfidf - vector])
-        # Assumed training events to be a list of event_info objects
-        training_events = np.array([event.description for event in training_events])
+    def train(self, training_events_dict, info_repo):
+        ## input : dict([user, list of events_ids])
+        events_info = info_repo["events_info"]
+        self.training_vecs = {}
+        training_events = np.array([events_info[event_id]["description"] for user_id in training_events_dict for event_id in training_events_dict[user_id]])
         self.word_tfidf.fit(training_events)
+        for user_id in training_events_dict:
+            training_event = ""
+            for event_id in training_events_dict[user_id]:
+                training_event += events_info[event_id]["description"]
+            training_vec = self.word_tfidf.transform(training_event)
+            self.training_vecs[user_id] = training_vec
+        #print self.word_tfidf.vocabulary
         pass
 
 #TODO - Transform - form vectors for a user's past events. Sum these vectors to form user vector. Calculate similarity scores and rank events.
 #TODO - Ideas for summing (direct sum, weighted sum(timed, event_importance)).
-    def test(self, member, potential_events):
+    def test(self, member_id, potential_events, info_repo):
         ## input : member_id, list_of_events
-        ## output : [event : cosine similarity score]
-        # Assumed member to be a member_info object and potential_events to be a list of event_info objects
-
-        member_desc = ""
-        for event in member.events:
-            member_desc += event.description
-
-        member_vec = self.word_tfidf.transform(member_desc)
-        test_events = np.array([event.description for event in potential_events])
+        ## output : [cosine similarity scores]
+        member_vec = self.training_vecs[member_id]
+        events_info = info_repo["events_info"]
+        test_events = np.array([events_info[event_id]["description"] for event_id in potential_events])
         test_events_vecs = self.word_tfidf.transform(test_events)
         similarity_scores = cosine_similarity(member_vec, test_events_vecs)
         return similarity_scores
