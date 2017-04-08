54ffe44870e8e9c7e4c6494f7f5725f2ca0f0da1
Aditya Nanjangud <aditya.ngud@gmail.com>
2017-04-08 14:53:59 -0500

    Dictionary for storing simscores.

--- src/content/content_recommender.py
+++ src/content/content_recommender.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from collections import defaultdict
 class ContentRecommender:
 
     def __init__(self):
@@ -13,7 +14,9 @@ class ContentRecommender:
         ## input : dict([user, list of events_ids])
         events_info = info_repo["events_info"]
         self.training_vecs = {}
-        print "Training events ", training_events_dict['11173777']
+
+        #TEST: Print training events for 11173777
+        #print "Training events ", training_events_dict['11173777']
         training_events = np.array([events_info[event_id]["description"] for user_id in training_events_dict for event_id in training_events_dict[user_id]])
         self.word_tfidf.fit(training_events)
         for user_id in training_events_dict:
@@ -24,7 +27,7 @@ class ContentRecommender:
             self.training_vecs[user_id] = training_vec
 
     # Transform - form vectors for a user's past events. Sum these vectors to form user vector. Calculate similarity scores and rank events.
-    def test(self, member_id, potential_events, info_repo):
+    def test(self, member_id, potential_events, info_repo, simscores):
         ## input : member_id, list_of_events
         ## output : [cosine similarity scores]
         member_vec = self.training_vecs[member_id]
@@ -32,9 +35,12 @@ class ContentRecommender:
         test_events = np.array([events_info[event_id]["description"] for event_id in potential_events])
         test_events_vecs = self.word_tfidf.transform(test_events)
         similarity_scores = cosine_similarity(member_vec, test_events_vecs).flatten()
-        args =  similarity_scores.argsort()[:-5:-1]
-        print "All event ids ", info_repo["members_events"]['11173777']
-        top_5_recommended_events = []
-        for i in args:
-            top_5_recommended_events.append(potential_events[i])
-        return top_5_recommended_events
+        for i in xrange(len(potential_events)):
+            simscores[member_id][potential_events[i]] = similarity_scores[i]
+
+        # TEST: Pick top 5 similar scores. Print all events. Print top 5 events.
+        #args =  similarity_scores.argsort()[:-5:-1]
+        #print "All event ids ", info_repo["members_events"]['11173777']
+        #top_5_recommended_events = []
+        #for i in args:
+        #    top_5_recommended_events.append(potential_events[i])
--- src/index.py
+++ src/index.py
@@ -4,11 +4,10 @@ from partition import *
 from content.content_recommender import ContentRecommender
 import datetime
 
-def content_classifier(repo, timestamp):
+def content_classifier(repo, timestamp, simscores):
     #Wrapper for content classification.
     #call train and test for all member and
     #events and print the results
-
     training_events_dict = get_member_events_dict_in_range(repo, timestamp - train_data_interval, timestamp)
 
     potential_events = filter_events_by_time_range(repo, list(repo['events_info'].keys()), timestamp,
@@ -16,7 +15,11 @@ def content_classifier(repo, timestamp):
 
     contentRecommender = ContentRecommender()
     contentRecommender.train(training_events_dict, repo)
-    print contentRecommender.test('11173777', potential_events, repo)
+
+    #TEST: Call test only for member_id 11173777
+    #contentRecommender.test('11173777', potential_events, repo, simscores)
+    for member_id in repo['members_info']:
+        contentRecommender.test(member_id, potential_events, repo, simscores)
 
 def main():
     parser = argparse.ArgumentParser(description='Run Event Recommender')
@@ -35,20 +38,24 @@ def main():
     repo['members_info'] = members_info
     repo['members_events'] = member_events
 
+    #simscores_across_features is a dictionary to store similarity score obtained for each feature
+    #for each member and for a given event. For example in case of content classifer we will
+    #access the similarity score as follows: simscores['content_classifier'][member_id][event_id].
+    #We will pass only a specific subdictionary (Ex: simscores['content_classifier']) to the
+    #classifier functions, which will work on them and populate them.
+    simscores_across_features = defaultdict(lambda :defaultdict(lambda :defaultdict(lambda :0)))
+
     start_time = 1262304000 # 1st Jan 2010
     end_time = 1451606400 # 1st Jan 2016
-
     timestamps = get_timestamps(start_time, end_time)
 
     for t in timestamps:
         print "Partition at timestamp ", datetime.datetime.fromtimestamp(t), " are : "
-
         partitioned_repo = get_partitioned_repo(t, repo)
-
 
         #Call content based classifer train and test functions from here. Pass the repo
         #as an argument to these functions.
-        content_classifier(partitioned_repo, t)
+        content_classifier(partitioned_repo, t, simscores_across_features['content_classifier'])
 
 if __name__ == "__main__":
     main()
