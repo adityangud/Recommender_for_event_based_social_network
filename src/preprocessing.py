244bc824d49e96f30593c0c1e57a2e2e294fcac3
navneet gupta <navgupta14@gmail.com>
2017-04-15 19:13:13 -0500

    1. Adding group frequency feature for recommendation. 2. Loading group for each event in preprocessing while loading_groups. 3. Adding event_group and group_events in partitioned repo.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,7 +4,12 @@ from helper import *
 def load_groups(group_members_file, group_events_file):
     group_members = read_json(group_members_file)
     group_events = read_json(group_events_file)
-    return group_members, group_events
+    event_groups = defaultdict()
+    for group_id in group_events:
+        event_ids = group_events[group_id]
+        for event_id in event_ids:
+            event_groups[event_id] = group_id
+    return group_members, group_events, event_groups
 
 
 def load_events(events_info_file):
@@ -22,5 +27,5 @@ def load_rsvps(rsvps_file):
         member_ids = event_rsvps[event]
         for member in member_ids:
             member_events[member].append(event)
-
     return member_events
+
