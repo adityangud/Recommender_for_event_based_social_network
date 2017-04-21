f47b83d0e6efc6acb4c682d4bb4894be93be3f68
Shubham Jain <shubham7jain@gmail.com>
2017-04-21 18:32:03 -0500

    Added for single member

--- src/hybrid/learning_to_rank.py
+++ src/hybrid/learning_to_rank.py
@@ -7,10 +7,10 @@ class LearningToRank:
     def learning_to_rank(self,simscores_across_features, events, members, hybrid_simscores):
 
         ### Basic Average of simscores
-        for member in members:
-            for event in events:
-                for feature in simscores_across_features:
-                    hybrid_simscores[member][event] += simscores_across_features[feature][member][event]
+        member = '12563492'
+        for event in events:
+            for feature in simscores_across_features:
+                hybrid_simscores[member][event] += simscores_across_features[feature][member][event]
 
-        args = sorted(hybrid_simscores['12563492'].items(), key=operator.itemgetter(1))[-6:-1]
-        print "Top 5 events recommended to user 12563492", args
+        args = sorted(hybrid_simscores[member].items(), key=operator.itemgetter(1))[-6:-1]
+        print "Top 5 events recommended to user ", member, " : ", args
