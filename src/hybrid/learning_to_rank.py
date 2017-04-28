ae06a8e270168840547e72f299beb6c6abb35bc8
Shubham Jain <shubham7jain@gmail.com>
2017-04-28 16:47:56 -0500

    Added multiple algos for learning to rank

--- src/hybrid/learning_to_rank.py
+++ src/hybrid/learning_to_rank.py
@@ -1,39 +1,28 @@
-import operator
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import precision_recall_fscore_support
+from sklearn.svm import LinearSVC
+
 
 class LearningToRank:
     def __init__(self):
         pass
 
-    def learning_to_rank(self,simscores_across_features, events, members, hybrid_simscores):
-
-        ### Basic Average of simscores
-        member_recommendations = {}
-        for member in members:
-            for event in events:
-                for feature in simscores_across_features:
-                    hybrid_simscores[member][event] += simscores_across_features[feature][member][event]
-            args = sorted(hybrid_simscores[member].items(), key=operator.itemgetter(1))
-            member_recommendations[member] = args
-        #print "events recommended to user : ", member, " in sorted order : ", args
-        return member_recommendations
-
-
-    def learning(self,simscores_across_features, test_events, all_members_rsvpd_events, test_members, f):
-        from sklearn import svm
-        import numpy as np
-        from sklearn.svm import LinearSVC
-        from sklearn.metrics import precision_recall_fscore_support
+    def learning(self,simscores_across_features, test_events, all_members_rsvpd_events, test_members, f, algolist, number_of_members):
         training_data_features_list = []
         train_y = []
+        training_members = int(0.8 * number_of_members)
         for feature in simscores_across_features:
             features = []
-            for member in test_members[:40]:
+            for member in test_members[:training_members]:
                 for event in test_events:
                     features.append(simscores_across_features[feature][member][event])
             final_features = np.array(features)
             training_data_features_list.append(final_features)
 
-        for member in test_members[:40]:
+        for member in test_members[:training_members]:
             for event in test_events:
                 if event in all_members_rsvpd_events[member]:
                     train_y.append(1)
@@ -47,13 +36,13 @@ class LearningToRank:
         test_y = []
         for feature in simscores_across_features:
             features = []
-            for member in test_members[40:]:
+            for member in test_members[training_members:]:
                 for event in test_events:
                     features.append(simscores_across_features[feature][member][event])
             final_features = np.array(features)
             test_data_features_list.append(final_features)
 
-        for member in test_members[40:]:
+        for member in test_members[training_members:]:
             for event in test_events:
                 if event in all_members_rsvpd_events[member]:
                     test_y.append(1)
@@ -62,19 +51,38 @@ class LearningToRank:
 
         final_test_data_features_list = np.column_stack(test_data_features_list)
         final_test_y = np.array(test_y)
-        #svm = svm.SVC(C=0.3, kernel='linear', probability=True, verbose=True)
-        svm = LinearSVC()
-        svm.fit(final_training_data_features_list, final_train_y)
-        predictions = svm.predict(final_test_data_features_list)
-        prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
-        for i, j in zip(final_test_y, predictions):
-            f.write(str(i) + str(j) + '\n')
-        f.write("==========================================================\n")
-        f.write("(PRf): " + str(prfs))
-        f.write("==========================================================\n")
-        f.write("Iteration done")
-        f.write("==========================================================\n")
-        print "score : ", svm.score(final_test_data_features_list, final_test_y)
-        print "(PRf): ", prfs
+        classification_algo = LinearSVC()
+        if 'svm' in algolist:
+            classification_algo = MLPClassifier()
+            classification_algo.fit(final_training_data_features_list, final_train_y)
+            predictions = classification_algo.predict(final_test_data_features_list)
+            prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
+            f.write(
+                "SVM -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]))
+            f.flush()
+        if 'mlp' in algolist:
+            classification_algo = MLPClassifier()
+            classification_algo.fit(final_training_data_features_list, final_train_y)
+            predictions = classification_algo.predict(final_test_data_features_list)
+            prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
+            f.write(
+                "MLP -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]))
+            f.flush()
+        if 'nb' in algolist:
+            classification_algo = GaussianNB()
+            classification_algo.fit(final_training_data_features_list, final_train_y)
+            predictions = classification_algo.predict(final_test_data_features_list)
+            prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
+            f.write(
+                "NB -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]))
+            f.flush()
+        if 'rf' in algolist:
+            classification_algo = RandomForestClassifier()
+            classification_algo.fit(final_training_data_features_list, final_train_y)
+            predictions = classification_algo.predict(final_test_data_features_list)
+            prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
+            f.write(
+                "RF -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]))
+            f.flush()
 
 
