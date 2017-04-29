eb074d81e5093f3b6d3fb2617b5758fa6c59802e
Shubham Jain <shubham7jain@gmail.com>
2017-04-29 12:31:59 -0500

    Added feature importance logic and create graphs for that

--- src/hybrid/learning_to_rank.py
+++ src/hybrid/learning_to_rank.py
@@ -4,18 +4,20 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.svm import LinearSVC
-
+import matplotlib.pyplot as plt
 
 class LearningToRank:
     def __init__(self):
         pass
 
-    def learning(self,simscores_across_features, test_events, all_members_rsvpd_events, test_members, f, algolist, number_of_members):
+    def learning(self,simscores_across_features, test_events, all_members_rsvpd_events, test_members, f, algolist, number_of_members, partition_number):
         training_data_features_list = []
         train_y = []
+        feature_names = []
         training_members = int(0.8 * number_of_members)
         for feature in simscores_across_features:
             features = []
+            feature_names.append(feature)
             for member in test_members[:training_members]:
                 for event in test_events:
                     features.append(simscores_across_features[feature][member][event])
@@ -51,38 +53,61 @@ class LearningToRank:
 
         final_test_data_features_list = np.column_stack(test_data_features_list)
         final_test_y = np.array(test_y)
-        classification_algo = LinearSVC()
+
+        classifier = LinearSVC()
         if 'svm' in algolist:
-            classification_algo = LinearSVC()
-            classification_algo.fit(final_training_data_features_list, final_train_y)
-            predictions = classification_algo.predict(final_test_data_features_list)
+            classifier = LinearSVC()
+            classifier.fit(final_training_data_features_list, final_train_y)
+
+            ## Finding important features
+            coef = classifier.coef_.ravel()
+            # create plot
+            plt.figure(partition_number, figsize=(5, 10))
+            ax = plt.subplot(211)
+            self.plot_feature_importance(ax, "Linear SVM classifier", coef, partition_number, feature_names)
+
+            predictions = classifier.predict(final_test_data_features_list)
             prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
             f.write(
                 "SVM -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]) + "\n")
             f.flush()
         if 'mlp' in algolist:
-            classification_algo = MLPClassifier()
-            classification_algo.fit(final_training_data_features_list, final_train_y)
-            predictions = classification_algo.predict(final_test_data_features_list)
+            classifier = MLPClassifier()
+            classifier.fit(final_training_data_features_list, final_train_y)
+            predictions = classifier.predict(final_test_data_features_list)
             prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
             f.write(
                 "MLP -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]) + "\n")
             f.flush()
         if 'nb' in algolist:
-            classification_algo = GaussianNB()
-            classification_algo.fit(final_training_data_features_list, final_train_y)
-            predictions = classification_algo.predict(final_test_data_features_list)
+            classifier = GaussianNB()
+            classifier.fit(final_training_data_features_list, final_train_y)
+            predictions = classifier.predict(final_test_data_features_list)
             prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
             f.write(
                 "NB -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]) + "\n")
             f.flush()
         if 'rf' in algolist:
-            classification_algo = RandomForestClassifier()
-            classification_algo.fit(final_training_data_features_list, final_train_y)
-            predictions = classification_algo.predict(final_test_data_features_list)
+            classifier = RandomForestClassifier()
+            classifier.fit(final_training_data_features_list, final_train_y)
+
+            ## Finding important features
+            importances = classifier.feature_importances_
+            ## Create plot
+            plt.figure(partition_number, figsize=(5, 10))
+            ax = plt.subplot(212)
+            self.plot_feature_importance(ax, "Random Forest Classifier", importances, partition_number, feature_names)
+
+            predictions = classifier.predict(final_test_data_features_list)
             prfs = precision_recall_fscore_support(final_test_y, predictions, labels=[0, 1])
             f.write(
                 "RF -> Precision : " + str(prfs[0][1]) + " Recall : " + str(prfs[1][1]) + " F-measure : " + str(prfs[2][1]) + "\n")
             f.flush()
+        plt.tight_layout()
+        plt.savefig("figures/feature_importance/" + str(partition_number) + "_partition.png")
 
-
+    def plot_feature_importance(self, ax, title, importances, partition_number, feature_names):
+        ax.set_title(title)
+        plt.bar(np.arange(4), importances, color='blue')
+        feature_names = np.array(feature_names)
+        plt.xticks(np.arange(0, 4) + 1.0 / 2, feature_names, rotation=60, ha='right')
